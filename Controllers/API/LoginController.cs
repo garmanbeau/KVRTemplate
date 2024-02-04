b0f6a4c007a0081d13035b3ec13087eea6a0fbff
garmanbeau <106044681+garmanbeau@users.noreply.github.com>
2024-02-04 16:23:45 -0600

    changes related to password hashing functionality.

---
+++ Controllers/API/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using KVRTemplate.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace KVRTemplate.Controllers.apis
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+
+        [HttpPost("register")]
+        public IActionResult Register(string username, string password)
+        {
+            try
+            {
+                //var response = await _userService.Register(resource, cancellationToken);
+                //return Ok(response);
+
+                string x = PasswordHasher.ComputeHash(password, "pepper", 3);
+
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { ErrorMessage = "hi" });
+            }
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] JsonElement body)
+        {
+            //TODO: get context and search for username
+            string username = (body.GetProperty("username")).ToString();
+            string password = (body.GetProperty("password")).ToString();
+
+            //if (user == null)
+            //    throw new Exception("Username or password did not match.");
+
+            string passwdHash = "zTYpKAMCoqYj12ahAoZU25RsI4XlPDizKX2nVphlnNzEXvkHqCZgGPrxDqx0W0ZDkXwJs8KGVE1b8mpU4BsG6Q==";
+
+            //TODO: get passwdHash from the db and compare it
+            string passwordHash = PasswordHasher.ComputeHash(password, "pepper", 3);
+            if (passwdHash != passwordHash) //TODO: password hash should be a variable
+                throw new Exception("Username or password did not match.");
+
+            return Ok();
+            //return whatever needs to do log in.
+        }
+    }
+}
---
+++ Services/PasswordHasher.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace KVRTemplate.Services
+{
+    public static class PasswordHasher
+    {
+        public static string ComputeHash(string password, string pepper, int iteration)
+        {
+            if (iteration <= 0) return password;
+
+            using var sha512 = SHA512.Create(); //declares and initializes sha512 object.
+
+            string passwordPepper = $"{password}{pepper}"; //combines password cleartext with pepper
+            byte[] byteValue = Encoding.UTF8.GetBytes(passwordPepper);//encode to a sequence of bytes
+            byte[] byteHash = sha512.ComputeHash(byteValue); //takes the bytes and hashes them
+            string hash = Convert.ToBase64String(byteHash); //convert back to string/characters from bytes.
+
+            return ComputeHash(hash, pepper, iteration - 1); //calls itself if iteration is not 0.
+        }
+    }
+}
